e8bbbe84e549fc1d9d77fed9a848f11f6622f3e8
Garrett Kucinski <garrettryan@doejo.com>
2016-09-26 10:58:42 -0500

    progress, saved options are overwriting the whole table

--- admin/partials/vsw_slider-admin-display.php
+++ admin/partials/vsw_slider-admin-display.php
@@ -16,20 +16,36 @@
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 
 <?php
+  $options = get_option( $this->plugin_name . '-settings' );
+  $slider_one_options = $options['slider_one'];
+  $counter = 0;
+  var_dump( $slider_one_options );
+?>
 
-$options = get_option( $this->plugin_name . '-settings' );
-
-var_dump( $options );
-
- ?>
-
-<div class="wrap">
+<div class='wrap'>
 
   <h1><?php echo $this->settings_title; ?> Settings</h1>
 
-  <form method="post" action="">
+  <form method='post' action=''>
     <?php settings_fields( $this->plugin_name . '-settings' ); ?>
-    <?php do_settings_sections( $this->plugin_name . '-settings' ); ?>
+    <table class="widefat form-table">
+    <?php foreach ( $slider_one_options as $option ) : ?>
+      <?php var_dump( $counter ); var_dump( $option ); ?>
+          <tr class='slide'>
+            <td>
+              <img class='' src=''>
+              <input type='button' name='<?php echo $this->plugin_name; ?>-settings[slider_one][slide_'<?php $counter; ?>'][slide_image]' class='button' value='upload image'>
+            </td>
+            <td>
+              <input type='text' name='<?php echo $this->plugin_name; ?>-settings[slider_one][slide_'<?php $counter; ?>'][slide_title]' class='regular-text' value='<?php echo $option['slide_title']; ?>'>
+            </td>
+            <td>
+              <input type='text' name='<?php echo $this->plugin_name; ?>-settings[slider_one][slide_'<?php $counter; ?>'][slide_link]' class='regular-text' value='<?php echo $option['slide_link']; ?>'>
+            </td>
+          </tr>
+          <?php $counter++; var_dump( $counter ); ?>
+    <?php endforeach; ?>
+    </table>
     <?php submit_button(); ?>
   </form>
 
